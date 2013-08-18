0d28366cf65a880a78154c310ba5034f9007ab23
shawn <shawn@s5.(none)>
2013-08-17 18:30:47 -0600

    updates

--- setup.sh
+++ setup.sh
@@ -38,7 +38,8 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+#git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/ruggershawn/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
